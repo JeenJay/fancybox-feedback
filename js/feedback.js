44109344bda51b1db9f659da3b0d44bad3112953
Dmitry Maksimov <jeendsgn@gmail.com>
2018-05-25 15:40:48 +0300

    Deploy project

---
+++ js/feedback.js
@@ -0,0 +1,61 @@
+function validateEmail(email) {
+	var reg = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return reg.test(email);
+}
+
+$(document).ready(function() {
+
+	$(".fancybox-modal").fancybox({
+		autoSize: false,
+		autoHeight: true,
+		width: 400
+	});
+
+	$("#feedback").submit(function() {
+		return false;
+	});
+
+	$('#send-message').on("click", function() {
+		var nameval = $('#user-name').val();
+		var emailval = $('#user-email').val();
+		// var msgval    = $("#msg").val();
+		var namelen    = nameval.length;
+		var mailvalid = validateEmail(emailval);
+		
+		if (mailvalid == false) {
+			$('#user-email').addClass('error');
+		}
+		else if (mailvalid == true) {
+			$('#user-email').removeClass('error');
+		}
+		
+		if (namelen < 3) {
+			$('#user-name').addClass('error');
+		}
+		else if (namelen >= 3) {
+			$('#user-name').removeClass('error');
+		}
+		
+		if(mailvalid == true && namelen >= 3) {
+			// если обе проверки пройдены
+			// сначала мы скрываем кнопку отправки
+			// $('#send-message').replaceWith("<em>отправка...</em>");
+			
+			$.ajax({
+				type: 'POST',
+				url: '../sendmessage.php',
+				data: $('#feedback').serialize(),
+				success: function (data) {
+					if (data == 'true') {
+						$('#feedback').fadeOut('fast', function () {
+							$(this).before('<p><strong>Успешно! Ваше сообщение отправлено  :)</strong></p>');
+							setTimeout('$.fancybox.close()', 1000);
+						});
+					}
+				}
+			});
+		}
+
+	});
+
+});
---
+++ sendmessage.php
@@ -0,0 +1,29 @@
+<?php
+
+	$sendto   = 'email@mail.ru';
+	$username = $_POST['user-name'];
+	$usermail = $_POST['user-email'];
+	$content  = nl2br($_POST['user-message']);
+
+	// Формирование заголовка письма
+	$subject  = 'Новое сообщение';
+	$headers  = 'From: ' . strip_tags($usermail) . '\r\n';
+	$headers .= 'Reply-To: ' . strip_tags($usermail) . '\r\n';
+	$headers .= 'MIME-Version: 1.0 \r\n';
+	$headers .= 'Content-Type: text/html;charset=utf-8 \r\n';
+
+	// Формирование тела письма
+	$msg  = '<html><body style="font-family:Arial,sans-serif;">';
+	$msg .= '<h2 style="font-weight:bold;border-bottom:1px dotted #ccc;">Новое сообщение</h2> \r\n';
+	$msg .= '<p><strong>От кого:</strong> ' . $username . '(' . $usermail . ')</p> \r\n';
+	$msg .= '<p><strong>Сообщение:</strong> ' . $content . '</p> \r\n';
+	$msg .= '</body></html>';
+
+	// отправка сообщения
+	if (@mail ($sendto, $subject, $msg, $headers)) {
+		echo 'true';
+	} else {
+		echo 'false';
+	}
+
+?>
